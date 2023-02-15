5a4aaa0b69c076e2085095366652def94861b2b7
Pragati00713 <kushwahapragati713@gmail.com>
2023-02-15 09:28:00 -0800

    1234

---
+++ anil.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"enter the value of number1 and number2:";
+    cin>>num1>>num2;
+    cout<<"the value of number1 is"<<num1<<" and number 2 is  "<<num 2;
+    
+}
